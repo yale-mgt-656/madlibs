109df73fd23c7bb1731f9c8595abe10a36f5ea1f
Griffin <griffin.solot-kehl@yale.edu>
2017-09-28 18:17:53 +0000

    Fixing to push to right branch with more adjectives

---
+++ pos/adjectives/griffin-kevin-adjectives.js
@@ -0,0 +1,14 @@
+module.exports.adjectives = [
+    'senile',
+    'bumpy',
+    'versitile',
+    'desperate',
+    'handsome',
+    'carping',
+    'purple',
+    'black',
+    'denim',
+    'matte white',
+    'Chinese',
+    'Chinese-American'
+];

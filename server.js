ac88265ef4785955eb19ff93cdce64873e689eb7
Kyle L. Jensen <kljensen@gmail.com>
2017-09-28 11:40:05 -0400

    Handle missing templates with 404

--- server.js
+++ server.js
@@ -9,31 +9,39 @@ var PORT = process.env.PORT || '3000';
 var IP = process.env.IP || '127.0.0.1';
 
 nunjucks.configure('stories', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 });
+app.set('view engine', 'html');
 
 var context = require('./lib/misc.js').getPOS(__dirname, 'pos');
 
 // When no template is specified, choose one randomly
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   var allLayouts = require('./lib/misc.js').requireLayout(__dirname, 'stories');
   res.render(_.sample(allLayouts), context);
 });
 
 app.get('/favicon.ico', function (req, res) {
   // Ignore favicon.
-  res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+  res.writeHead(200, {
+    'Content-Type': 'image/x-icon'
+  });
   res.end();
   return;
 });
 
 // Route to a particular template if one is specified
-app.get('/:template', function(req, res) {
-  res.render(req.params.template, context);
+app.get('/:template', function (req, res) {
+  res.render(req.params.template, context, (err, html) => {
+    if (err) {
+      res.status(404).send('Not found');
+    } else {
+      res.send(html);
+    }
+  });
 });
 
-
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(PORT, IP, function () {
   console.log('Example app listening at http://%s:%s', IP, PORT);
 });
